3b14c06e30e7b2a7d809b792eda5de77ce7de319
Yanik Bittel <yanik.bittel@unil.ch>
2025-11-28 17:48:34 +0000

    Update models.py: added Random Forrest Model

--- src/models.py
+++ src/models.py
@@ -3,6 +3,7 @@ from typing import Tuple, List
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
 SEED: int = 42
 
@@ -50,4 +51,44 @@ def train_logistic_regression(df: pd.DataFrame, target: str = "sovereign_crisis"
     model.fit(X_train, y_train)
     # Large dataset, so I increased max_iter to ensure convergence
     # random_state for reproducibility, ensuring consistent results across runs
+    return model, X_test, y_test
+
+##### Train random forest model #####
+def train_random_forest(
+    df: pd.DataFrame,
+    target: str = "sovereign_crisis",) -> Tuple[RandomForestClassifier, pd.DataFrame, pd.Series]:
+    """Train a Random Forest classifier."""
+
+    # same feature engineering / cleaning as in train_logistic_regression
+    feature_cols = get_feature_columns(df, target) # get feature columns excluding target and id columns
+    X = df[feature_cols].copy() # features dataframe, again, copying to avoid modifying original df
+    y = df[target].copy() # target series
+
+    # Convert all features to numeric, coerce errors to NaN
+    X = X.apply(pd.to_numeric, errors="coerce") # convert features to numeric data types
+
+    # Drop rows where target is NaN
+    mask = y.notna() # mask for non-missing target values
+    X = X[mask] # keep only rows in X where y is not NaN
+    y = y[mask] # keep only non-missing values in y
+
+    # Drop rows with any NaN in features
+    X = X.dropna()  # drop rows with missing feature values
+    y = y.loc[X.index] # align y with cleaned X
+
+    # Train/test split
+    X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=SEED, stratify=y) # 20% test size, 80% training size, SEED for reproducibility, stratify to maintain class distribution
+
+    ### The model training
+    model = RandomForestClassifier(
+        n_estimators=300,
+        max_depth=None,
+        min_samples_split=2,
+        min_samples_leaf=1,
+        random_state=SEED,
+        n_jobs=-1,
+        class_weight="balanced")
+
+    model.fit(X_train, y_train) # fit the Random Forest model to training data
+
     return model, X_test, y_test
